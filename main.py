ba8fa53b030037ab9ca0c7b78901dfe06bab01a8
Tim Konick <konick781@gmail.com>
2013-04-28 21:08:24 -0400

    first

---
+++ main.py
@@ -0,0 +1,52 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import argparse
+from lib.mail_commands import Mail_Pool
+from lib.mail_carrier import Carrier
+
+
+def options():
+    parser = argparse.ArgumentParser(description='''MailBand is an utility to help manage multiple e-mail accounts.  
+                                                    MailBand is capable of reading and writing emails from your shell.
+                                                    MailBand can back-up all your emails locally on your storage device.  
+                                                    MailBand is compatible with: -hotmail -live -gmail -yahoo -msn''')
+    p = parser.add_mutually_exclusive_group()
+    p.add_argument('-a', '--add_acnt', help='add an e-mail account', action='store_true')
+    p.add_argument('-c', '--show_acnts', help='list current email accounts', action='store_true')
+    p.add_argument('-r', '--remove_acnt', help='remove an e-mail account', action='store_true')
+    p.add_argument('-R', '--read', help='show all e-mails in accounts', action='store_true')
+    p.add_argument('-F', '--fetch_stored', help='show all e-mail currently stored locally', action='store_true')
+    p.add_argument('-D', '--delete_stored', help='delete selected e-mails from local storage', action='store_true')
+    p.add_argument('-d', '--delete', help='delete selected e-mails', action='store_true')
+    p.add_argument('-s', '--send', help='send an e-mail from selected account', action='store_true')
+    p.add_argument('-w', '--write', help='write selected e-mails to local storage', action='store_true')
+    parse = parser.parse_args()
+    return parse
+
+def main(opt):
+    pool = Mail_Pool()
+    if opt.add_acnt:
+        pool.add_account()
+    if opt.remove_acnt:
+        pool.remove_account()
+    if opt.show_acnts:
+        pool.show_current_accounts()
+    else:
+        choices = pool.mail_box()
+        if opt.read:
+            Carrier.deliver(choices, 'read')
+        if opt.delete:
+            Carrier.deliver(choices, 'delete')
+        if opt.send:
+            return
+        if opt.write:
+            Carrier.deliver(choices, 'write')
+        if opt.fetch_stored:
+            pool.fetch_stored(choices)
+        if opt.delete_stored:
+            return
+
+if __name__ == '__main__':
+    opt = options()
+    main(opt)
